92141c04a2cfd62c3e8b779cedbdad4c9b9e99a8
nicolasrannou <npm.rannou@gmail.com>
2014-02-13 14:54:31 -0500

    test deployement on github

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+function init() {
+
+    console.log('\'Allo \'main!');
+    
+    var sd2 = new Slicedrop2();
+    sd2.init();
+
+}
+
+window.addEventListener('DOMContentLoaded', init, false);
